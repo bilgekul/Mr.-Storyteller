4f9af07fc85a37958f4f2c436e644c12ad4dc759
Bilgekul <b.kadirhanofficial@hotmail.com>
2023-12-16 13:26:44 +0300

    "changes"

--- storytelling.py
+++ storytelling.py
@@ -3,3 +3,12 @@ from langchain.llms.openai import OpenAI
 from langchain.chains import SequentialChain, LLMChain
 import os
 from dotenv import load_dotenv,find_dotenv
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.prompts import PromptTemplate
+
+_ = load_dotenv()
+
+api_key = os.environ["OPENAI_API_KEY"]
+
+st.set_page_config(page_title="Mr StoryTeller")
